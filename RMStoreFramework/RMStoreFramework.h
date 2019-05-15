d252c74c8d266e2752b6d8ccefdfd4ce5641701e
Bruno Virlet <bvirlet@users.noreply.github.com>
2019-05-15 09:25:49 +0200

    Carthage support

---
+++ RMStoreFramework/RMStoreFramework.h
@@ -0,0 +1,25 @@
+//
+//  RMStoreFramework.h
+//  RMStoreFramework
+//
+//  Created by Bruno Virlet on 5/15/19.
+//  Copyright © 2019 Robot Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RMStoreFramework.
+FOUNDATION_EXPORT double RMStoreFrameworkVersionNumber;
+
+//! Project version string for RMStoreFramework.
+FOUNDATION_EXPORT const unsigned char RMStoreFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMStoreFramework/PublicHeader.h>
+
+#import <RMStoreFramework/RMAppReceipt.h>
+#import <RMStoreFramework/RMStoreAppReceiptVerifier.h>
+#import <RMStoreFramework/RMStoreKeychainPersistence.h>
+#import <RMStoreFramework/RMStoreTransaction.h>
+#import <RMStoreFramework/RMStoreTransactionReceiptVerifier.h>
+#import <RMStoreFramework/RMStoreUserDefaultsPersistence.h>
+#import <RMStoreFramework/RMStore.h>
