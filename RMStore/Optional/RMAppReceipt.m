b92a9405eae3b0b07880a990f59218d8bb29610d
Bruno Virlet <bruno.virlet@gmail.com>
2019-11-15 11:52:40 +0100

    Fix headers for Carthage build

--- RMStore/Optional/RMAppReceipt.m
+++ RMStore/Optional/RMAppReceipt.m
@@ -20,10 +20,10 @@
 
 #import "RMAppReceipt.h"
 #import <UIKit/UIKit.h>
-#import <openssl/pkcs7.h>
-#import <openssl/objects.h>
-#import <openssl/sha.h>
-#import <openssl/x509.h>
+#import <OpenSSL/pkcs7.h>
+#import <OpenSSL/objects.h>
+#import <OpenSSL/sha.h>
+#import <OpenSSL/x509.h>
 
 // From https://developer.apple.com/library/ios/releasenotes/General/ValidateAppStoreReceipt/Chapters/ReceiptFields.html#//apple_ref/doc/uid/TP40010573-CH106-SW1
 NSInteger const RMAppReceiptASN1TypeBundleIdentifier = 2;
